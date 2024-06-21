bb234766ee9a03e42138b3ce0a62d12b0fae7893
ken-muiruri07 <kenmuiruri07@gmail.com>
2024-06-21 13:10:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import turtle as t
+from turtle import Turtle, Screen
+import random
+tim = Turtle()
+tim.shape("turtle")
+t.colormode(255)
+tim.speed("fastest")
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_colour = (r, g, b)
+    return random_colour
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/ size_of_gap)):
+        tim.pencolor(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+
+screen = Screen()
+screen.exitonclick()
+
+
